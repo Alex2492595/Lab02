901cfe42f2131bb6a6fda8dda0daf0d2a173d07f
2492595 <2492595@host.docker.internal>
2025-09-10 10:11:30 -0400

    Initial Commit: Created basic JavaFX setup and added images

---
+++ src/lab02/Lab02.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab02;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2492595
+ */
+public class Lab02 extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
+        
+        Scene scene = new Scene(root, 250, 300);
+        
+        primaryStage.setTitle("Java Games");
+        primaryStage.show();
+    }
+}
